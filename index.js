c0e98045635c17cb0e8c9037be1c3d3d422df11f
Andika <andikasaputra.id@gmail.com>
2020-05-29 03:00:04 +0700

    initial array

---
+++ index.js
@@ -0,0 +1,25 @@
+var angkaArr = [0, 1, 2, 3, 4, 9, 2, 0];
+var kataArr = ["aku", "dia", "mereka", "kami"];
+var siswaArr = [
+    {
+        id: 1,
+        nama: "Andika",
+        nilai: 80
+    },
+    {
+        id: 2,
+        nama: "Wulandari",
+        nilai: 76
+    },
+    {
+        id: 3,
+        nama: "Fitri",
+        nilai: 90
+    },
+    {
+        id: 4,
+        nama: "Januar",
+        nilai: 54
+    },
+];
+console.log(angkaArr);
